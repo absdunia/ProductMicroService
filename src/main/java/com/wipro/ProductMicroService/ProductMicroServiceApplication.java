7d096ff255e3a64ee6db38713a5dcd0c2d8a628b
Abhishek Roy <roy.of.abhishek13@gmail.com>
2019-10-20 16:17:41 +0530

    Update Swagger for Product

--- src/main/java/com/wipro/ProductMicroService/ProductMicroServiceApplication.java
+++ src/main/java/com/wipro/ProductMicroService/ProductMicroServiceApplication.java
@@ -4,13 +4,26 @@ package com.wipro.ProductMicroService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableSwagger2
 public class ProductMicroServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductMicroServiceApplication.class, args);
 	}
+	
+	@Bean
+    public Docket inventoryApi() {
+      return new Docket(DocumentationType.SWAGGER_2).select()
+         .apis(RequestHandlerSelectors.basePackage("com.wipro.ProductMicroService")).build();
+    }
 
 }
